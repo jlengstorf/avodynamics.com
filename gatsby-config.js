8c080bbccd1b3ae9be4bf5b38946b60fe9602d39
amns-llourcey <samuellane.lourcey@arcelormittal.com>
2021-05-11 07:13:55 -0500

    Setup for gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,11 +29,15 @@ module.exports = {
         showSpinner: false,
       },
     },
-    // {
-    //   resolve: 'gatsby-plugin-google-analytics',
+    // { TODO: Add Tracking ID
+    //   resolve: `gatsby-plugin-google-gtag`,
     //   options: {
-    //     trackingId: config.googleAnalyticsID,
-    //     head: true,
+    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
+    //     trackingIds: [
+    //       "GA-TRACKING_ID", // Google Analytics / GA
+    //       "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+    //       "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+    //     ],
     //   },
     // },
     // { TODO: Use plugin-manifest instead
