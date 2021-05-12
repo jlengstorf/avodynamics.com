79be668237aef7cd7df0491c58db81cb8db11471
amns-llourcey <samuellane.lourcey@arcelormittal.com>
2021-05-12 07:11:53 -0500

    Adds client login link

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -5,7 +5,7 @@ import { Wrapper } from './styles';
 const NavbarLinks = ({ desktop }) => (
   <Wrapper desktop={desktop}>
     <Link to="/contact">Contact</Link>
-    <a href="">Login</a>
+    <a href="https://client.avodynamics.com/public/client/portal/5f90a43c896cad116427ca89">Login</a>
   </Wrapper>
 );
 
